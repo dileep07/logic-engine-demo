24aa50e833d03c59c935b760d06f873cc6e46b54
Dileep <dileep07@live.in>
2025-06-04 23:51:42 +1000

    First commit

---
+++ src/main/java/com/dil/logicengine/LogicengineApplication.java
@@ -0,0 +1,13 @@
+package com.dil.logicengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogicengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogicengineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dil/logicengine/LogicengineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dil.logicengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogicengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
